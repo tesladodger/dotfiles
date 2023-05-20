e5f0633e4e9fa9a579b125d41b3082d711987a28
tesladodger <andrerfmartins@protonmail.com>
2023-05-20 15:27:28 +0100

    Redefine telescope mappings with functions

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,15 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<C-p>', ':Telescope git_files hidden=true<CR>', {})
-vim.keymap.set('n', '<leader>pf', ':Telescope find_files hidden=true no_ignore=true<CR>', {})
+vim.keymap.set('n', '<C-p>', function()
+    builtin.git_files({ hidden = true })
+end)
+
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({ hidden = true, no_ignore = true })
+end)
+
 vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
+
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input('Grep > ') })
 end)
