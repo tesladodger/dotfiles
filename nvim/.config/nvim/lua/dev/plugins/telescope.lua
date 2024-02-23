e4d0b67155c3385e4d10cf6664af3625b3c18a45
tesladodger <andrerfmartins@protonmail.com>
2024-02-23 14:33:45 +0100

    Add `marks.nvim`

--- nvim/.config/nvim/lua/dev/plugins/telescope.lua
+++ nvim/.config/nvim/lua/dev/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     },
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.x',
+        branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             require('telescope').setup({
