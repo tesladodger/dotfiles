6d148c814f9619def790680e1b71b99b8131c040
tesladodger <andrerfmartins@protonmail.com>
2024-07-09 10:50:59 +0200

    Add 'jump to implementation' mapping

--- nvim/.config/nvim/lua/tesla/plugins/lsp.lua
+++ nvim/.config/nvim/lua/tesla/plugins/lsp.lua
@@ -31,6 +31,7 @@ return {
             local opts = { buffer = bufnr, remap = false }
 
             vim.keymap.set('n', '<leader>gd', function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set('n', '<leader>gi', function() vim.lsp.buf.implementation() end, opts)
             vim.keymap.set('n', '<leader>h', function() vim.lsp.buf.hover() end, opts)
 
             vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
